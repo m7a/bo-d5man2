24e2e4213dc483c6f2fd80d456881fcb4e54c561
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2024-02-10 22:13:16 +0100

    Got interaction with subprocess to work correctly by patching the cecho package c-parts.

--- d5mantui4/src/d5mantui4_input.erl
+++ d5mantui4/src/d5mantui4_input.erl
@@ -5,5 +5,15 @@ start(NotifyUI) ->
 	{ok, spawn_link(?MODULE, run, [NotifyUI])}.
 
 run(NotifyUI) ->
-	gen_server:cast(NotifyUI, {getch, cecho:getch()}),
+	case cecho:getch() of
+	16#0a ->
+		% Upon reading a newline (\n, ENTER) halt processing until
+		% the UI returns because it may spawn an exteranl program and
+		% we should only resume our input reading after it has ended.
+		% This is modelled by the synchronous call rather than cast in
+		% event of newline characters.
+		ok = gen_server:call(NotifyUI, {getch, 16#0a}, infinity);
+	OtherCharacter ->
+		gen_server:cast(NotifyUI, {getch, OtherCharacter})
+	end,
 	run(NotifyUI).
